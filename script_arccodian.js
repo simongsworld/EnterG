1cc8ee57d7dfd5291ddd9fa99602ecd489428b64
simongsworld <reason0227@naver.com>
2024-12-19 12:50:17 +0900

    Update script_arccodian.js

--- script_arccodian.js
+++ script_arccodian.js
@@ -1,10 +1,9 @@
 <script>
-    document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.accordion-header').forEach(header => {
     header.addEventListener('click', () => {
       const content = header.nextElementSibling;
 
-      // 토글 방식으로 드롭다운 열고 닫기
       if (content.style.display === 'block') {
         content.style.display = 'none';
       } else {
