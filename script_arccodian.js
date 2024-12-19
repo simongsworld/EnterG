6ac3226f2fda10fc82ad1f3f021256cbca86208f
simongsworld <reason0227@naver.com>
2024-12-19 12:44:49 +0900

    Update script_arccodian.js

--- script_arccodian.js
+++ script_arccodian.js
@@ -1,14 +1,16 @@
 <script>
-    document.querySelectorAll('.accordion-header').forEach(header => {
-      header.addEventListener('click', () => {
-        const content = header.nextElementSibling;
+    document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+      const content = header.nextElementSibling;
 
-        // 토글 방식으로 드롭다운 열고 닫기
-        if (content.style.display === 'block') {
-          content.style.display = 'none';
-        } else {
-          content.style.display = 'block';
-        }
-      });
+      // 토글 방식으로 드롭다운 열고 닫기
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
     });
+  });
+});
   </script>
